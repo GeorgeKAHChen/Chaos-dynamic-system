905b04275a7eef54667c07562578fc5de920064e
KazukiAmakawa <KazukiAmakawa@gmail.com>
2021-02-26 20:17:07 +0600

    mandelbolt

--- code/section4/mandelbort.py
+++ code/section4/mandelbort.py
@@ -1,11 +1,10 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import random
 import Init
 
-total_iteration_time = 100
+total_iteration_time = 1000
 interval_para = [[-1.5, 0.6], [-1, 1]]
-delta_para = 0.001
+#delta_para = 0.0005
+delta_para = 0.0001
 initial_val = [0, 0]
 inf_val = 100
 
@@ -18,20 +17,27 @@ def f(x, parameter):
 def main():
     para_x = np.arange(interval_para[0][0], interval_para[0][1], delta_para)
     para_y = np.arange(interval_para[1][0], interval_para[1][1], delta_para)
-    img = [[0 for n in range(len(para_y))] for n in range(len(para_x))]
+    img = []
     for i in range(0, len(para_x)):
+        img_x = []
         for j in range(0, len(para_y)):
             print(i, j, len(para_x), len(para_y), end = "\r")
             x = initial_val
+            add_var = True
             for kase in range(0, total_iteration_time):
                 x = f(x, [para_x[i], para_y[j]])
                 if x[0] > inf_val or x[1] > inf_val or x[0] < -inf_val or x[1] < -inf_val:
-                    img[i][j] = 255
+                    img_x.append([255, 255, 255])
+                    add_var = False
                     break
+            if add_var:
+                img_x.append([0, 0, 0])
+        img.append(img_x)
     print()
     img = np.float32(img)
     #print()
-    Init.ImageIO(file_dir = "./img.png", img = np.float32(img), io = "o", mode = "grey", backend = "opencv")
+    #print(img)
+    Init.ImageIO(file_dir = "./img.png", img = np.float32(img), io = "o", mode = "grey", backend = "Pillow")
 
 
 if __name__ == '__main__':
--- code/section4/sierpinski_carpet.py
+++ code/section4/sierpinski_carpet.py
@@ -2,42 +2,51 @@ import Init
 import numpy as np
 
 FILE_DIR = "./img"
-iteration_time = 10
+iteration_time = 9
 
 def main():
-    for kase in range(1, iteration_time + 1):
-        vals = [[[0]] for n in range(pow(pow(3, kase), 2))]
-        for i in range(0, len(vals)):
-            have_five = False
-            new_i = i
-            while 1:
-                code = new_i - 9 * int(new_i/9)
-                if code == 4:
-                    have_five = True
-                    break
-                if int(new_i/9) == 0:
-                    break
-                new_i = int(new_i/9)
-
-            if have_five:
-                vals[i][0][0] = 255
-
-        mark = 0
+    kase = iteration_time
+    vals = [[[0]] for n in range(pow(pow(3, kase), 2))]
+    for i in range(0, len(vals)):
+        print(i, len(vals), end = "\r")
+        have_five = False
+        new_i = i
         while 1:
-            if mark + 1 >= len(vals):
+            code = new_i - 9 * int(new_i/9)
+            if code == 4:
+                have_five = True
                 break
-            matrix = vals[mark: mark + 9]
-            new_matrix = [[0 for n in range(len(vals[mark]) * 3)] for n in range(len(vals[mark]) * 3)]
-            for p in range(0, 3):
-                for q in range(0, 3):
-                    for i in range(0, len(vals[mark])):
-                        for j in range(0, len(vals[mark])):
-                            new_matrix[p * len(vals[mark]) + i][q * len(vals[mark]) + j] = matrix[p * 3 + q][i][j]
-            mark += 9
-            vals.append(new_matrix)
-        Filename = FILE_DIR + str(kase) + ".png"
-        img = vals[len(vals) - 1]
-        Init.ImageIO(file_dir = Filename, img = np.float32(img), io = "o", mode = "grey", backend = "opencv")
+            if int(new_i/9) == 0:
+                break
+            new_i = int(new_i/9)
+
+        if have_five:
+            vals[i][0][0] = 255
+    print()
+    mark = 0
+    while 1:
+        print(mark, len(vals), end = "\r")
+        if mark + 1 >= len(vals):
+            break
+        matrix = vals[mark: mark + 9]
+        new_matrix = [[0 for n in range(len(vals[mark]) * 3)] for n in range(len(vals[mark]) * 3)]
+        for p in range(0, 3):
+            for q in range(0, 3):
+                for i in range(0, len(vals[mark])):
+                    for j in range(0, len(vals[mark])):
+                        new_matrix[p * len(vals[mark]) + i][q * len(vals[mark]) + j] = matrix[p * 3 + q][i][j]
+        mark += 9
+        vals.append(new_matrix)
+    print()
+    Filename = FILE_DIR + str(kase) + ".png"
+    img = vals[len(vals) - 1]
+    for i in range(0, len(img)):
+        for j in range(0, len(img[i])):
+            if img[i][j] == 0:
+                img[i][j] = [0, 0, 0]
+            else:
+                img[i][j] = [255, 255, 255]
+    Init.ImageIO(file_dir = Filename, img = np.float32(img), io = "o", mode = "grey", backend = "Pillow")
     return
 
 
