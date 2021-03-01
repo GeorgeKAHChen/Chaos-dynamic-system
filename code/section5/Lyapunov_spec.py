e19d900191c1e8925942179faf664d2ba7d9c4c0
KazukiAmakawa <KazukiAmakawa@gmail.com>
2021-03-01 23:12:36 +0600

    Lyapunov spec finished, TODO:PROOF

--- code/section5/Lyapunov_spec.py
+++ code/section5/Lyapunov_spec.py
@@ -4,7 +4,12 @@ from scipy.integrate import odeint
 import random
 from scipy import linalg
 import copy
+
 COLOR_LOOP = ["r", "g", "b", "c", "m"]
+
+
+
+
 
 #=========================================
 #
@@ -23,7 +28,7 @@ beta = 8.0 / 3.0
 
 #Delta_t = 0.001
 #Delta_t = 1
-Delta_t = 0.01
+Delta_t = 0.0001
 initial_value = [1.0, 1.0, 1.0]
 x_axis = np.arange(0.0, 50.0, Delta_t)
 
@@ -47,8 +52,6 @@ def states():
 
 
 
-
-
 #=========================================
 #
 #   Henon's model
@@ -56,7 +59,7 @@ def states():
 #
 #=========================================
 """
-iteration_total = 1000
+iteration_total = 50000
 initial_value = [random.random(), random.random()]
 x_axis = np.arange(0, iteration_total + 1, 1)
 a = 1.4
@@ -76,8 +79,11 @@ def states():
     for i in range(0, iteration_total):
         states.append(f(states[len(states) - 1]))
     return states
-
 """
+
+
+
+
 
 #=========================================
 #
@@ -86,7 +92,7 @@ def states():
 #
 #=========================================
 """
-iteration_total = 1000
+iteration_total = 10000
 initial_value = [random.random()]
 x_axis = np.arange(0, iteration_total + 1, 1)
 a = 4
@@ -110,61 +116,115 @@ def states():
 
 
 
+#=========================================
+#
+#   Rossler system
+#
+#
+#=========================================
+"""
+a = 0.2
+b = 0.3
+c = 10
+
+
+Delta_t = 0.0001
+initial_value = [1.0, 1.0, 1.0]
+x_axis = np.arange(0.0, 50.0, Delta_t)
+
+def f(state, t):
+    x, y, z = state
+    return - y - z, x + a * y, b + z * (x - c)
+
+def Jf(state):
+    x, y, z = state
+    #return Delta_t * np.matrix([[-sigma, sigma, 0], [(rho - z), -1, -x], [y, x, -beta]]) + np.eye(3)
+    return np.matrix([[1,               -Delta_t,                   -Delta_t], 
+                      [Delta_t,         a * Delta_t + 1 ,           0], 
+                      [Delta_t * z,     0,                          (x - c) * Delta_t + 1]])
+
+
+def states():
+    return odeint(f, initial_value, x_axis)
+"""
 
 
 
+
+
+#=========================================
+#
+#   Duffing system
+#
+#
+#=========================================
+"""
+val_alpha = 1
+val_beta = 0.04
+val_gamma = 1
+val_delta = 0.1
+val_omega = np.pi / 2
+
+
+Delta_t = 0.0001
+initial_value = [1.0, 1.0]
+x_axis = np.arange(0.0, 50.0, Delta_t)
+
+def f(state, t):
+    x, y = state
+    return y, val_gamma * np.cos(val_omega * t) - val_alpha * x - val_beta * x * x *x - val_delta * y
+
+def Jf(state):
+    x, y = state
+    return np.matrix([[1,                                               -Delta_t                ], 
+                      [(-val_alpha - 3 * val_beta * x * x) * Delta_t,   val_delta * Delta_t + 1 ]])
+
+
+def states():
+    return odeint(f, initial_value, x_axis)
+"""
+
+
+
+
+
+
+"""
+#   Main function: Lyapunov Spec
+"""
 
 
 def main():
-    """         
-    curr_states = states()
-    Lyapunov_spec = []
-    e = np.array([0 for n in range(len(initial_value))])
-    for kase in range(0, len(curr_states)):
-        print(kase, len(curr_states), end = "\r")
-        Jacobian = Jf(curr_states[kase])
-        eig_val, eig_vec = np.linalg.eig(Jacobian)
-        for ttl in range(0, len(initial_value)):
-            e[ttl] += np.log(np.abs(np.real(eig_val[ttl])))
-        Lyapunov_spec.append(np.power(e, 1/(kase+1)))
-    print()
-    print(Lyapunov_spec)
-    print()
-    val_x = x_axis
-    plt.grid(True)
-    for i in range(0, len(initial_value)):
-        val = []
-        for j in range(0, len(Lyapunov_spec)):
-            val.append(Lyapunov_spec[j][i])
-        plt.plot(val_x, val, COLOR_LOOP[i % len(COLOR_LOOP)])
-
-    plt.show()  
-    
-    """
-
-
     curr_states = states()
     output_vals = [[0 for n in range((len(initial_value)))]]
     orth_mat = np.eye(len(initial_value))
     for kase in range(0, len(curr_states) - 1):
         Jacobian = np.matrix(Jf(curr_states[kase]))
-        orth_mat = np.matrix(linalg.orth(np.matrix(Jacobian * np.matrix(orth_mat))))
+        tmp = np.matrix(Jacobian) * np.matrix(orth_mat)
+        orth_mat = np.matrix(linalg.orth(tmp))
         new_output = copy.deepcopy(output_vals[len(output_vals) - 1])
-        for i in range(0, len(initial_value)):
-            norm = np.linalg.norm(orth_mat[:, i])
-            new_output[i] += ((new_output[i] * kase) + np.log(norm)) / (kase + 1)
+        for i in range(0, len(new_output)):
+            norm = np.linalg.norm(tmp[:, i])
+            new_output[i] = ((new_output[i] * (x_axis[kase] - x_axis[0])) + np.log(norm)) / (x_axis[kase + 1] - x_axis[0])
         output_vals.append(new_output)
 
-
+    fig = plt.gcf()
+    fig.set_size_inches(25, 3)
     output_vals = np.matrix(output_vals)
-    val_x = x_axis
     plt.grid(True)
     for i in range(0, len(initial_value)):
         val = np.array(output_vals[:, i].reshape(-1).reshape(-1))
-        print(val[0])
-        print(val_x)
-        plt.plot(val_x, val[0], COLOR_LOOP[i % len(COLOR_LOOP)])
+        plt.plot(x_axis, val[0], COLOR_LOOP[(i + 1) % len(COLOR_LOOP)])
 
+    sum_val = []
+    for i in range(0, len(output_vals)):
+        sum_val.append(np.sum(output_vals[i][0]))
+    plt.plot(x_axis, sum_val, COLOR_LOOP[0])
+
+    output_str = "Output Vals\nLyapunov vals: \t"
+    output_str += str(output_vals[len(output_vals) - 1])
+    output_str += "\n sum: " + str(sum_val[len(sum_val) - 1])
+    print(output_str)
     plt.show()  
     
     
